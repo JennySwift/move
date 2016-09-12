88dc6dbfd696eb89147ba4f5fc7db78a9de3d6a3
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-12 11:52:44 +1000

    Update navbar

--- resources/views/shared/header/navbar-component.blade.php
+++ resources/views/shared/header/navbar-component.blade.php
@@ -14,30 +14,7 @@
 
             @else
 
-            {{--<li id="menu-dropdown" class="dropdown">--}}
-            {{--<a href="#" class="dropdown-toggle fa fa-bars" data-toggle="dropdown"><span class="caret"></span></a>--}}
-            {{--<ul class="dropdown-menu" role="menu">--}}
-            {{--<li><a href="/auth/logout">logout</a></li>--}}
-            {{--</ul>--}}
-            {{--</li>--}}
-
-                    <!-- <li class="desktop"><a ng-click="changeTab('entries')" href="">home</a></li>
-        <li class="iphone"><a ng-click="changeTab('food_entries')" href="">food log</a></li>
-        <li class="iphone"><a ng-click="changeTab('exercise_entries')" href="">exercise log</a></li> -->
-
             <li><a v-link="{path: '/'}" href="#" class="fa fa-home"></a></li>
-
-            <li id="menu-dropdown" class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="fa fa-cutlery"></span>
-                    <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a v-link="{path: '/foods'}" href="#">Foods</a></li>
-                    <li><a v-link="{path: '/recipes'}" href="#">Recipes</a></li>
-                    <li><a v-link="{path: '/food-units'}" href="#v-link="{path: '/exercises'}" ">Units</a></li>
-                </ul>
-            </li>
 
             <li id="menu-dropdown" class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -47,23 +24,6 @@
                 <ul class="dropdown-menu" role="menu">
                     <li><a v-link="{path: '/exercises'}" href="#">Exercises</a></li>
                     <li><a v-link="{path: '/exercise-units'}" href="#">Units</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a v-link="{path: '/journal'}" href="#" class="fa fa-pencil"></a>
-            </li>
-
-            <li id="menu-dropdown" class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="fa fa-clock-o"></span>
-                    <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a v-link="{path: '/timers'}" href="#">Timers</a></li>
-                    <li><a v-link="{path: '/activities'}" href="#">Activities</a></li>
-                    <li><a href="#" v-link="{path: '/timers/new-manual'}" v-on:click="showNewManualTimerPopup()">New Manual Timer</a></li>
-                    {{--<li><a v-link="{path: '/graphs'}" href="#">Graphs</a></li>--}}
                 </ul>
             </li>
 

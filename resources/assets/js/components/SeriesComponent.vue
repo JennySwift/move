e495be4349a0b8eaedba350e8c4dc5cdd0c723bc
Jenny Swift <cheezyspaghetti@gmail.com>
2017-03-30 20:04:01 +1100

    Frontend for updating series color

--- resources/assets/js/components/SeriesComponent.vue
+++ resources/assets/js/components/SeriesComponent.vue
@@ -1,8 +1,21 @@
 <template>
     <div id="series-page">
-        <ul v-for="series in shared.exerciseSeries"class="list-group">
-            <li v-bind:style="{background: series.color}" v-bind:class="{'has-color': series.color}" class="list-group-item series">{{series.name}}</li>
-        </ul>
+
+        <div
+            v-for="series in shared.exerciseSeries"
+            class="pointer exercise card"
+        >
+            <div v-bind:style="{background: series.color}" v-bind:class="{'has-color': series.color}" class="card-header">
+                {{series.name}}
+            </div>
+            <div class="card-block">
+                <!--<h4 class="card-title exercise-name">{{ exercise.name }}</h4>-->
+                <!--<p class="card-text text-left"></p>-->
+
+                <button class="btn btn-default" v-on:click="updateSeries(series)">Edit</button>
+            </div>
+
+        </div>
 
     </div>
 
@@ -14,11 +27,33 @@
         data: function () {
             return {
                 shared: store.state,
+                selectedSeries: {}
             };
         },
         components: {},
         methods: {
 
+
+            /**
+            *
+            */
+            updateSeries: function (series) {
+                this.selectedSeries = series;
+                var data = {
+                    color: prompt("Enter a color.")
+                };
+
+                helpers.put({
+                    url: '/api/exerciseSeries/' + this.selectedSeries.id,
+                    data: data,
+                    property: 'exerciseSeries',
+                    message: 'Series updated',
+                    redirectTo: this.redirectTo,
+                    callback: function (response) {
+
+                    }.bind(this)
+                });
+            }
         },
         ready: function () {
 

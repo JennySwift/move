a59412e33aa2efb28e3f3a52cfc44675a30b4a7a
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-07 12:38:40 +1000

    Add meta tags for mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script>
         window.Laravel = {!! json_encode([

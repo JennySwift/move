64b964ec5fd1d4c47bad4059f69c764aecbeee3e
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-07 14:00:27 +1000

    Styling

--- resources/assets/js/components/shared/ButtonsComponent.vue
+++ resources/assets/js/components/shared/ButtonsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="buttons-container">
         <router-link
             v-if="redirectTo"
             :to="redirectTo"
@@ -39,5 +39,17 @@
 </script>
 
 <style lang="scss" type="text/scss">
-
+    .buttons-container {
+        display: flex;
+        > * {
+            flex-grow: 1;
+            margin: 0 5px;
+            &:first-child {
+                margin-left: 0px;
+            }
+            &:last-child {
+                margin-right: 0px;
+            }
+        }
+    }
 </style>

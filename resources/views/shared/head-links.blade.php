ee60be0ddb16470293bdd5b77ff7f3971170fb22
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-10 10:36:07 +1000

    Use cache busting

--- resources/views/shared/head-links.blade.php
+++ resources/views/shared/head-links.blade.php
@@ -1,4 +1,5 @@
 <link href='https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope' rel='stylesheet' type='text/css'>
 <link href="https://fonts.googleapis.com/css?family=Gothic+A1|Lato:300|Montserrat" rel="stylesheet">
 <link rel="stylesheet" href="/css/fontawesome-all.min.css">
-<link rel="stylesheet" href="{{ elixir("css/app.css") }}">
+
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sourceMaps()
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}

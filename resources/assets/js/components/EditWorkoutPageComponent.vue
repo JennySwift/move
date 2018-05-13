94283a0d0641125b9ccd8ee8605d1664d333f963
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-13 11:10:29 +1000

    Use action sheets for exercises on workout page

--- resources/assets/js/components/EditWorkoutPageComponent.vue
+++ resources/assets/js/components/EditWorkoutPageComponent.vue
@@ -23,8 +23,7 @@
                         <div class="data-table-title">{{exercise[0].name}}</div>
                         <!-- Table actions -->
                         <div class="data-table-actions">
-                            <f7-link><f7-icon f7="time"></f7-icon></f7-link>
-                            <f7-link v-on:click="addSet(exercise[0])"><f7-icon f7="add"></f7-icon></f7-link>
+                            <f7-button v-on:click="openActions(exercise)">Actions</f7-button>
                         </div>
                     </div>
                     <!-- Card Content -->
@@ -120,6 +119,32 @@
             }
         },
         methods: {
+            openActions: function (exerciseGroup) {
+                var that = this;
+                var ac1 = app.f7.actions.create({
+                    buttons: [
+                        {
+                            text: 'Add Set',
+                            onClick: function () {
+                                that.addSet(exerciseGroup[0])
+                            }
+                        },
+                        {
+                            text: 'View History'
+                        },
+                        {
+                            text: 'View Exercise',
+                            onClick: function () {
+                                helpers.goToRoute('/exercises/' + exerciseGroup[0].exercise_id);
+                            }
+                        },
+                        {
+                            text: 'Cancel',
+                            color: 'red'
+                        },
+                    ]
+                }).open();
+            },
             isEmpty: function (obj) {
                 return _.isEmpty(obj);
             },

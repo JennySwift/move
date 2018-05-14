33321b4fa0eef543893fed30ec83d9ec84afce47
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-14 16:10:58 +1000

    Try and make build faster, and use hot module replacement

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,12 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sourceMaps()
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false
+    });
+
+// mix.options({ processCssUrls: false });
 
 if (mix.inProduction()) {
     mix.version();

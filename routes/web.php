231f1f4879f16694e5281942076e81118fa3beee
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-14 23:19:37 +1000

    Add logout route

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::group(['middleware' => ['auth', 'owner'], 'namespace' => 'API', 'prefix' 
 });
 
 Auth::routes();
+Route::get('/logout', 'Auth\LoginController@logout');
 
 Route::get('/home', 'HomeController@index')->name('home');

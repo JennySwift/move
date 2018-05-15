0d7777c77a7c93a6dd885b7e25f54ea4b51298f9
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-15 19:48:07 +1000

    Use versioning in dev environment

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sourceMaps()
+    .version()
    .sass('resources/assets/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false
@@ -20,6 +21,6 @@ mix.js('resources/assets/js/app.js', 'public/js')
 
 // mix.options({ processCssUrls: false });
 
-if (mix.inProduction()) {
-    mix.version();
-}
+// if (mix.inProduction()) {
+//     mix.version();
+// }

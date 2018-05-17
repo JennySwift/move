0f3c201779d803e129ce4e5689048291cf7aa135
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-17 13:35:37 +1000

    Switch menu to panel and add dummy links to panel

--- resources/assets/js/components/shared/NavbarComponent.vue
+++ resources/assets/js/components/shared/NavbarComponent.vue
@@ -8,17 +8,19 @@
         </f7-nav-left>
         <f7-nav-title>{{title}}</f7-nav-title>
         <f7-nav-right>
-            <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" :popover-open="'#' + popoverId + '-menu'"></f7-link>
-            <f7-popover :id="popoverId + '-menu'">
-                <f7-list contacts-list>
-                    <f7-list-group>
-                        <f7-list-item class="panel-close" link="/sessions" title="Sessions" popover-close></f7-list-item>
-                        <f7-list-item class="panel-close" link="/workouts" title="Workouts" popover-close></f7-list-item>
-                        <f7-list-item class="panel-close" link="/exercises/" title="Exercises" popover-close></f7-list-item>
-                        <slot></slot>
-                    </f7-list-group>
-                </f7-list>
-            </f7-popover>
+            <!--<f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" :popover-open="'#' + popoverId + '-menu'"></f7-link>-->
+            <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open></f7-link>
+
+            <!--<f7-popover :id="popoverId + '-menu'">-->
+                <!--<f7-list contacts-list>-->
+                    <!--<f7-list-group>-->
+                        <!--<f7-list-item class="panel-close" link="/sessions" title="Sessions" popover-close></f7-list-item>-->
+                        <!--<f7-list-item class="panel-close" link="/workouts" title="Workouts" popover-close></f7-list-item>-->
+                        <!--<f7-list-item class="panel-close" link="/exercises/" title="Exercises" popover-close></f7-list-item>-->
+                        <!--<slot></slot>-->
+                    <!--</f7-list-group>-->
+                <!--</f7-list>-->
+            <!--</f7-popover>-->
         </f7-nav-right>
     </f7-navbar>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,30 @@
 <div id="app">
     <f7-statusbar></f7-statusbar>
 
+    <f7-panel right cover>
+        <f7-block>
+            <f7-list contacts-list>
+                <f7-list-group>
+                    <f7-list-item class="panel-close" link="/sessions" title="Sessions"></f7-list-item>
+                    <f7-list-item class="panel-close" link="/workouts" title="Workouts"></f7-list-item>
+                    <f7-list-item class="panel-close" link="/exercises/" title="Exercises"></f7-list-item>
+                </f7-list-group>
+            </f7-list>
+        </f7-block>
+
+        <f7-block>
+            <f7-list contacts-list>
+                <f7-list-group>
+                    <f7-list-item class="panel-close" link="/workouts" title="Help"></f7-list-item>
+                    <f7-list-item class="panel-close" link="/sessions" title="Privacy"></f7-list-item>
+                    <f7-list-item class="panel-close" link="/workouts" title="Credits"></f7-list-item>
+                    <f7-list-item class="panel-close" link="/exercises/" title="jennyswiftcreations.com"></f7-list-item>
+                </f7-list-group>
+            </f7-list>
+        </f7-block>
+
+    </f7-panel>
+
     <f7-view id="main-view" main>
 
     </f7-view>

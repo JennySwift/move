fb530b997cfb10327d11d9b9a2960588808b1717
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-17 12:20:07 +1000

    Remove unwanted auth routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,24 @@ Route::group(['middleware' => ['auth', 'owner'], 'namespace' => 'API', 'prefix' 
 
 });
 
-Auth::routes();
+//Auth::routes();
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/logout', 'Auth\LoginController@logout');
+
+// Registration Routes...
+//$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+//$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
 
 Route::get('/home', 'HomeController@index')->name('home');

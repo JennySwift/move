d857278c7d7877fe9a0329ba3bd19ff0d0aaf5ed
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-17 13:45:30 +1000

    Get links to my home site working in panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,10 +22,9 @@
         <f7-block>
             <f7-list contacts-list>
                 <f7-list-group>
-                    <f7-list-item class="panel-close" link="/workouts" title="Help"></f7-list-item>
-                    <f7-list-item class="panel-close" link="/sessions" title="Privacy"></f7-list-item>
-                    <f7-list-item class="panel-close" link="/workouts" title="Credits"></f7-list-item>
-                    <f7-list-item class="panel-close" link="/exercises/" title="jennyswiftcreations.com"></f7-list-item>
+                    <f7-list-item class="panel-close" external link="https://jennyswiftcreations.com/privacy-policy" title="Privacy"></f7-list-item>
+                    <f7-list-item class="panel-close" external link="https://jennyswiftcreations.com/credits" title="Credits"></f7-list-item>
+                    <f7-list-item class="panel-close" external link="https://jennyswiftcreations.com" title="jennyswiftcreations"></f7-list-item>
                 </f7-list-group>
             </f7-list>
         </f7-block>

990fcbc7a9ec39c37a55549fc50aeb4ffacb4654
Jenny Swift <cheezyspaghetti@gmail.com>
2018-05-19 12:03:39 +1000

    Cleanup

--- resources/assets/js/components/ExercisesPageComponent.vue
+++ resources/assets/js/components/ExercisesPageComponent.vue
@@ -45,48 +45,12 @@
         template: '#exercises-page-template',
         data: function () {
             return {
-                date: store.state.date,
-                exerciseSeriesHistory: [],
-                showNewSeriesFields: false,
-                showNewExerciseFields: false,
-                selectedSeries: {
-                    exercises: {
-                        data: []
-                    }
-                },
-                showExerciseEntryInputs: false,
                 shared: store.state,
                 baseUrl: 'api/series'
             };
         },
-        computed: {
-            selectedExercise: function () {
-                return this.shared.selectedExercise;
-            },
-            units: function () {
-                return this.shared.exerciseUnits;
-            },
-            programs: function () {
-                return this.shared.programs;
-            },
-            exerciseSeries: function () {
-                return this.shared.exerciseSeries;
-            },
-            filters: function () {
-                return this.shared.exerciseFilters;
-            }
-        },
         components: {
             'filters': ExerciseFiltersComponent
-        },
-        // page events
-        on: {
-            pageInit(event, pageData) {
-                // do something on page init
-            },
-            pageAfterOut(event, pageData) {
-                // do something when page transitioned out of view
-            },
         },
         methods: {
             /**
@@ -96,104 +60,6 @@
                 store.set(exercise, 'exercise');
             },
 
-            /**
-             *
-             */
-            insertDefaultExerciseSet: function (exercise) {
-                store.insertExerciseSet(exercise, true);
-            },
-
-            /**
-             *
-             */
-            insertExerciseSet: function (exercise) {
-                var quantity = prompt("How many " + exercise.defaultUnit.data.name + "?");
-                if (quantity) {
-                    store.insertExerciseSet(exercise, false, quantity);
-                }
-            },
-
-            /**
-             * For the series filter
-             * @param series
-             * @returns {*}
-             */
-            moveLastDoneNullToEnd: function (series) {
-                //Get the series that have lastDone null values
-                var seriesWithNullLastDone = _.filter(series, function (oneSeries) {
-                    return oneSeries.lastDone == null;
-                });
-
-                //Remove the series that have lastDone null values
-                for (var i = 0; i < seriesWithNullLastDone.length; i++) {
-                    var index = _.indexOf(series, _.findWhere(series, {id: seriesWithNullLastDone[i].id}));
-                    series = _.without(series, series[index]);
-                }
-
-                //Add the series that have lastDone null values back on the
-                //end of the series array
-                for (var i = 0; i < seriesWithNullLastDone.length; i++) {
-                    series.push(seriesWithNullLastDone[i]);
-                }
-
-                return series;
-            },
-
-            /**
-            *
-            */
-            getExerciseSeriesHistory: function (key) {
-                //Find the series. The exercises were grouped according to series, so all we have is the series name (key).
-                var series = _.find(this.exerciseSeries, function (series) {
-                    return series.name === key;
-                });
-
-                helpers.get({
-                    url: 'api/seriesEntries/' + series.id,
-//                    storeProperty: 'exercises',
-//                    loadedProperty: 'exercisesLoaded',
-                    callback: function (response) {
-                        //For displaying the name of the series in the popup
-                        this.selectedSeries = series;
-                        this.exerciseSeriesHistory = response.data;
-                        $.event.trigger('show-series-history-popup');
-                    }
-                }.bind(this));
-            },
-
-            /**
-            *
-            */
-            getExercisesInSeries: function (series) {
-                helpers.get({
-                    url: this.baseUrl + '/' + series.id,
-                    storeProperty: 'series',
-                    loadedProperty: 'seriesLoaded',
-                    callback: function (response) {
-                        this.selectedSeries = response;
-                    }
-                }.bind(this));
-            },
-
-            /**
-             *
-             */
-            showExerciseSeriesPopup: function (key) {
-                //Find the series. The exercises were grouped according to series, so all we have is the series name (key).
-                var series = _.find(this.exerciseSeries, function (series) {
-                    return series.name === key;
-                });
-
-                helpers.get({
-                    url: this.baseUrl + '/' + series.id,
-                    storeProperty: 'series',
-                    loadedProperty: 'seriesLoaded',
-                    callback: function (response) {
-                        this.selectedSeries = response;
-                        $.event.trigger('show-series-popup');
-                    }
-                }.bind(this));
-            },
             listen: function () {
                 $('.ptr-content').on('ptr:refresh', function (e) {
                     store.getExercises({pullToRefresh: true});
